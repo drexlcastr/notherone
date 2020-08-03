f91693f8b8a68fef89ac15a3c2567b7652dc4002
Wayne Peck <wpeck@BNB-DEV-WPECK.local>
2020-08-02 20:33:04 -0700

    Test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NotherOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

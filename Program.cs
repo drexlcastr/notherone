1e9ec141aed45c1c446605242247351821a5bc4b
Wayne Peck <wpeck@BNB-DEV-WPECK.local>
2020-08-02 20:33:43 -0700

    Added Manny

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace NotherOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!! Manny!");
         }
     }
 }
